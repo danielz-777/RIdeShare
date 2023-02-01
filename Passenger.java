a30cac445ac2c75002e106ff8bc1a940998409d0
Daniel Zheng <65824308+danielz-777@users.noreply.github.com>
2023-02-01 10:23:59 -0800

    Made the Passegenger class

---
+++ Passenger.java
@@ -0,0 +1,27 @@
+public class Passenger
+{
+    //lowest level file for RideShare project
+    //contains Passenger's desired destination and current location
+
+    private int destination;
+    private int location;
+
+    public Passenger(int myDestination, int myLocation)
+    {
+        destination = myDestination;
+        location = myLocation;
+    }
+
+    //get destination 
+    public int getDestination()
+    {
+        return destination;
+    }
+
+    //get location;
+    public int getLocation()
+    {
+        return location;
+    }
+
+}
